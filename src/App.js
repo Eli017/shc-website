0b2466087e26551f8317732e4e4170d74f4e3ce1
elisokeland <esokeland@developertown.com>
2020-08-13 17:40:01 -0400

    Added Firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/main.scss";
-import Header from "/components/Header";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 const App = () => {
