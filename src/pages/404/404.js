c1a5ee4a43c12ec9baa8fc5cf8ce2607c5aff8bb
elisokeland <esokeland@developertown.com>
2020-08-18 15:19:01 -0400

    Finished 404 page

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./404.scss";
 
 const Page404 = () => {
-  return <p>404</p>;
+  return (
+    <main className={"page404"}>
+      <h1>Sorry!</h1>
+      <h2>Page Not Found</h2>
+    </main>
+  );
 };
 
 export default Page404;
