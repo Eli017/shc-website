53da551a8680a5dd540f3bcc98530f71450723ae
Eli <elisokeland@yahoo.com>
2020-08-24 21:55:14 -0400

    Fixed ESLint errors (Windows)

--- src/components/OfficerBoard/OfficerBoard.js
+++ src/components/OfficerBoard/OfficerBoard.js
@@ -12,11 +12,36 @@ const OfficerBoard = () => {
   return (
     <section className={"officerBoard"}>
       <OfficerCard name={"Zach Johnson"} position={"President"} photo={ZachJohnson} link={"/officers/president"} />
-      <OfficerCard name={"Tanner Prewitt"} position={"Vice President"} photo={TannerPrewitt} link={"/officers/vice-president"} />
-      <OfficerCard name={"Rebecca Conrad"} position={"Director of Administrative Affairs"} photo={RebeccaConrad} link={"/officers/records"} />
-      <OfficerCard name={"Kyra Christiansen"} position={"Director of Awards and Engagement"} photo={KyraChristiansen} link={"/officers/awards"} />
-      <OfficerCard name={"Eli Sokeland"} position={"Director of Online Operations"} photo={EliSokeland} link={"/officers/online"} />
-      <OfficerCard name={"Bobbie Burton"} position={"Director of Promotions"} photo={BobbieBurton} link={"/officers/promotions"} />
+      <OfficerCard
+        name={"Tanner Prewitt"}
+        position={"Vice President"}
+        photo={TannerPrewitt}
+        link={"/officers/vice-president"}
+      />
+      <OfficerCard
+        name={"Rebecca Conrad"}
+        position={"Director of Administrative Affairs"}
+        photo={RebeccaConrad}
+        link={"/officers/records"}
+      />
+      <OfficerCard
+        name={"Kyra Christiansen"}
+        position={"Director of Awards and Engagement"}
+        photo={KyraChristiansen}
+        link={"/officers/awards"}
+      />
+      <OfficerCard
+        name={"Eli Sokeland"}
+        position={"Director of Online Operations"}
+        photo={EliSokeland}
+        link={"/officers/online"}
+      />
+      <OfficerCard
+        name={"Bobbie Burton"}
+        position={"Director of Promotions"}
+        photo={BobbieBurton}
+        link={"/officers/promotions"}
+      />
     </section>
   );
 };
