25a8481a718a9e454fb399de3dccb07264925f52
Eli <elisokeland@yahoo.com>
2020-08-27 23:25:41 -0400

    Added firebase configuration files and added firebase secrets to github CI

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+export const auth = firebase.auth();
