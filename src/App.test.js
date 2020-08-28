efdf07f31366f1149082cd2b1095978137e839f3
Eli Sokeland <elisokeland@yahoo.com>
2020-08-28 01:34:04 -0400

    Fixed App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { axe, toHaveNoViolations } from "jest-axe";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { ModalProvider } from "./contexts/useModal";
+
+/*Jest-axe checks for a11y accessibility violations, which should be checked per page.
+This doesn't cover all accessibility, but it does hit a lot of common errors.
+This is proper way to test for these violations, do not deviate from its asynchronous nature.
+ */
+expect.extend(toHaveNoViolations);
+
+test("renders without a11y violations", async () => {
+  const { container } = render(
+    <ModalProvider>
+      <App />
+    </ModalProvider>,
+  );
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
