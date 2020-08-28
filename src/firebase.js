13f8231e324a6f045285b18c439ff6a6a2969429
Eli Sokeland <elisokeland@yahoo.com>
2020-08-28 00:50:26 -0400

    Added database test for firebase integration.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase";
 
+//Configuration based off of necessary Firebase information
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -8,7 +9,9 @@ const config = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
+//Initialize firebase here to prevent repetitive instantiation
 firebase.initializeApp(config);
 
-export const database = firebase.database();
+//Declare and export firebase functions and variables that are needed within the app
+export const database = firebase.firestore();
 export const auth = firebase.auth();
