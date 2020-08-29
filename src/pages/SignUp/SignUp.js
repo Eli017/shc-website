b489aabcdd6ef368284976770a75981bb7aefb84
Eli Sokeland <elisokeland@yahoo.com>
2020-08-29 18:19:25 -0400

    Add document reference to users collection for Firebase

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -24,6 +24,7 @@ const SignUp = () => {
           setFirebaseMessage("User Already Exists!");
         } else {
           userRef
+            .doc(email)
             .set({
               email: email,
               name: name,
