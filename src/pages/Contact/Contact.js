ea588e0ae345aa199d6bc5722585057236485bf0
Eli Sokeland <etsokeland@bsu.edu>
2020-09-08 01:23:26 -0400

    Added construction warning and styling to contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,16 @@
 import React from "react";
+import * as styles from "./contact.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import ConstructionWarning from "../../components/ConstructionWarning/ConstructionWarning";
 
 const Contact = () => {
   return (
     <React.Fragment>
       <Header />
-      <main>Contact</main>
+      <main className={styles.contact}>
+        <ConstructionWarning />
+      </main>
       <Footer />
     </React.Fragment>
   );
