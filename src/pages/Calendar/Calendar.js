6070a83aada4ddac40514e496ca4a30b25ad4180
Eli Sokeland <etsokeland@bsu.edu>
2020-09-08 01:14:15 -0400

    Added construction warning and styling to calendar page

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,12 +1,16 @@
 import React from "react";
+import * as styles from "./calendar.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import ConstructionWarning from "../../components/ConstructionWarning/ConstructionWarning";
 
 const Calendar = () => {
   return (
     <React.Fragment>
       <Header />
-      <main>Calendar</main>
+      <main className={styles.calendar}>
+        <ConstructionWarning />
+      </main>
       <Footer />
     </React.Fragment>
   );
